dbadb7ec2cfd4635186afacd5115ec4a8aa086a5
Frank Hesse <45906155+metalsonic21@users.noreply.github.com>
2020-08-02 15:55:59 -0400

    i'm sad

---
+++ src/components/commons/Navigation.vue
@@ -0,0 +1,8 @@
+<template>
+<div>
+    <v-app-bar color="teal lighten-3" height="60px">
+        <a href="/" class="brand-logo mt-2">
+            <img src="@/assets/logo.png" class="responsive-img mt-2" width="85px" height="85px" />
+        </a>
+    </v-app-bar>
+</div>
